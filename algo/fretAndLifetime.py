cd287859a9ff3c88c588c33058c2516ba2d2a251
Kan Liu <liukan@126.com>
2017-09-14 19:00:15 +0800

    Tau D

--- algo/fretAndLifetime.py
+++ algo/fretAndLifetime.py
@@ -16,6 +16,7 @@ except ImportError:
 
 import matplotlib
 from array import array
+from scipy.optimize import leastsq
 
 import matplotlib.cm as cm
 from PyQt5 import QtWidgets
@@ -89,9 +90,77 @@ class MatplotlibWidget(FigureCanvasQTAgg):
         self.updatingLine=not self.updatingLine
 
 
-def FretAndLifetime(dbname,burst,bins=(25,25),bgrate=None):
+def FretAndLifetime(burst,bins=(25,25),bgrate=None,burstD=4.1,bgrateD=None):
     #conn = sqlite3.connect(dbname)
     #c = conn.cursor()
+    Tau_D=1e-9
+    if type(burstD) is float:
+        Tau_D=burstD*1e-9
+    else:
+        lenburstD=len(burstD["All"]['chl'])
+        burstTauD = array("d")#np.zeros(lenburst)
+        weiD = array("d")#np.zeros(lenburst)
+            
+        for i in range(lenburstD):
+    #        c.execute("select Dtime,ch from fretData_All where TimeTag>=? and TimeTag<= ?",
+    #                  (burst["All"].stag[i],burst["All"].etag[i]))
+    #        data=c.fetchall()
+            data=burstD["All"]['chl'][i]
+            if bgrateD==None:
+                tt=burstD["All"]['timetag'][i]
+                #print(tt)
+                backgT=burstD["All"]['burstW'][i]/2+tt.iloc[0]*bgrate["SyncResolution"] #中点时刻
+                bgAA=BurstSearch.getBGrateAtT(bgrateD,"AexAem",backgT)
+                bgDD=BurstSearch.getBGrateAtT(bgrateD,"DexDem",backgT)
+                bgDA=BurstSearch.getBGrateAtT(bgrateD,"DexAem",backgT)            
+            w=len(data)       
+            nda=0#ch1
+            ndd=0;#ch2
+            naa=0;#ch3
+            nad=0#ch4        
+            sumdtimed=array("d")
+            for idxd in range(w):
+                d=data.iloc[idxd]
+                if d==1:
+                    nda+=1
+                elif d==2:
+                    ndd+=1
+                    sumdtimed.append(burstD["All"]['dtime'][i].iloc[idxd]*burstD["DelayResolution"]*1e9)
+                elif d==3:
+                    naa+=1
+                elif d==4:
+                    nad+=1                
+            if bgrateD==None:
+                naa=naa-bgAA*burstD["All"]['burstW'][i]
+                ndd=ndd-bgDD*burstD["All"]['burstW'][i]
+                nda=nda-bgDA*burstD["All"]['burstW'][i]
+                if naa< bgAA*burstD["All"]['burstW'][i] or ndd<0 or nda<0:
+                    continue
+            weiD.append(w)
+            Tau=np.mean(sumdtimed)
+            burstTauD.append(Tau)
+        [hist,bin]=np.histogram(burstTauD, bins=200)
+        x=array("d")
+        y=array("d")
+        sidx=np.argmax(hist) #只拟合最大值之后的数据
+        lenhist=len(hist)
+        for vh in range(sidx,lenhist):
+            if hist[vh]>0:        
+                y.append(np.log(hist[vh]))
+                x.append(bin[vh])            
+        m = 2  #多项式的次数
+        #先随机产生一组多项式分布的参数
+        p0 = np.random.randn(m)
+        plsq = leastsq(BGrate.residuals, p0, args=(y, x))
+        print(plsq)
+        # fig = plt.figure()
+        # ax = fig.add_subplot(111)
+        # n, bins, patches = plt.hist(burstTauD, 200, facecolor='g', alpha=0.75)
+        # plt.legend(loc='best')
+        # plt.show()
+        Tau_D=-1*plsq[0][0]*1e-9#
+        Tau_D=np.mean(burstTauD)*1e-9
+    print('Tau_D:',Tau_D)
     lenburst=len(burst["All"]['chl'])
     print("lenburst:",lenburst)
     burstFRET = array("d")#np.zeros(lenburst)
@@ -152,13 +221,12 @@ def FretAndLifetime(dbname,burst,bins=(25,25),bgrate=None):
         nad=0#ch4
         sumdtimed=array("d")
         for idxd in range(w):
-            d=data.iloc[idxd]
-            dtime=burst["All"]['dtime'][i].iloc[idxd]*burst["DelayResolution"]
+            d=data.iloc[idxd]            
             if d==1:
                 nda+=1
             elif d==2:
                 ndd+=1
-                sumdtimed.append(dtime)
+                sumdtimed.append(burst["All"]['dtime'][i].iloc[idxd]*burst["DelayResolution"])
             elif d==3:
                 naa+=1
             elif d==4:
@@ -170,27 +238,28 @@ def FretAndLifetime(dbname,burst,bins=(25,25),bgrate=None):
             ndd=ndd-bgDD*burst["All"]['burstW'][i]
             nda=nda-bgDA*burst["All"]['burstW'][i]
             if naa< bgAA*burst["All"]['burstW'][i] or ndd<0 or nda<0:
-                continue
-        wei.append(w)
-        Tau=sum(sumdtimed)/len(sumdtimed)/(4.1e-9)
-        burstTau.append(Tau)
-        burst["All"]['lifetime'][i]=Tau        
-        gamma=0.31        
-        beta=1.42
-        if (nda+ndd)==0:
-            burstFRET.append(1)
-            burst["All"]['e'][i]=1
-        else:
-            theFret=(nda)/(nda+gamma*ndd)
-            burstFRET.append(theFret)
-            burst["All"]['e'][i]=theFret
-        if (nda+ndd+naa)==0:
-            burstSeff.append(1)
-            burst["All"]['s'][i]=1
-        else:
-            theFret=(nda+gamma*ndd)/(nda+gamma*ndd+naa/beta)
-            burstSeff.append(theFret)
-            burst["All"]['s'][i]=theFret
+                continue        
+        Tau=np.mean(sumdtimed)/(Tau_D)
+        if Tau<=100:
+            wei.append(w)
+            burstTau.append(Tau)
+            burst["All"]['lifetime'][i]=Tau        
+            gamma=0.31        
+            beta=1.42
+            if (nda+ndd)==0:
+                burstFRET.append(1)
+                burst["All"]['e'][i]=1
+            else:
+                theFret=(nda)/(nda+gamma*ndd)
+                burstFRET.append(theFret)
+                burst["All"]['e'][i]=theFret
+            if (nda+ndd+naa)==0:
+                burstSeff.append(1)
+                burst["All"]['s'][i]=1
+            else:
+                theFret=(nda+gamma*ndd)/(nda+gamma*ndd+naa/beta)
+                burstSeff.append(theFret)
+                burst["All"]['s'][i]=theFret
 
     H, xedges, yedges = np.histogram2d(burstFRET,burstTau, bins=bins, weights=wei)
     #print(burstTau[0:100])
@@ -322,10 +391,13 @@ if __name__ == '__main__':
     dbname='E:/liuk/proj/ptu/data/55.sqlite'
     #dbname='E:/sf/oc/data/38.sqlite'
     dbname="/home/liuk/proj/data/RSV89C224C.sqlite"
+    dbTau_D="/home/liuk/proj/data/Tau_D.sqlite"
     br=BGrate.calcBGrate(dbname,20,400)
     burst=BurstSearch.findBurst(br,dbname,["All"])
+    brD=BGrate.calcBGrate(dbTau_D,20,400)
+    burstD=BurstSearch.findBurst(br,dbTau_D,["All"])
 
-    burstSeff, burstFRET,wei,H,xedges, yedges=FretAndLifetime(dbname,burst,(70,70),br)
+    burstSeff, burstFRET,wei,H,xedges, yedges=FretAndLifetime(burst,(70,70),br,burstD,brD)
 
     # with open('E:/tmp/objs.pickle', 'wb') as f:  # Python 3: open(..., 'wb')
     #     pickle.dump([burstSeff, burstFRET,wei,H,xedges], f)
